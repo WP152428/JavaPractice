b1c4c295d9a4bae95decc3b9b2b6196eef1e0bc2
ggoyang <ggoyang@218.38.137.28>
2017-04-05 10:15:46 +0900

    가독성을올리는코드작성

--- JavaPractice/src/org/dimigo/oop/Question.java
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -22,22 +22,22 @@ public class Question {
 	public static void main(String[] args) {
 		String[] Q ={"1.가장 좋아하는 가수는?","2.가장 좋아하는 배우는?","3.가장 좋아하는 과목은?"};
 		String[] A ={"빅뱅","조인성","응용 프로그래밍"};
-		String system;
 		Scanner scanner = new Scanner(System.in);
+		
 		for(int i=0;i<=2;i++){
 			System.out.println(Q[i]);
 			String answer = scanner.nextLine();
-			System.out.println(answer.equals(A[i]) ? "정답입니다!":"틀렸습니다!");
+			System.out.println(A[i].equals(answer) ? "정답입니다!":"틀렸습니다!");
 		}
 		scanner.close();
 		
     	StringBuilder sb = new StringBuilder();
 	    for(int i = 0;i<=2;i++){
 	    	sb
-	    		.append(Q[i])
-	    		.append(" ")
-	    		.append(A[i])
-	    		.append("입니다.\n");
+	        .append(Q[i])
+	    	.append(" ")
+	    	.append(A[i])
+	    	.append("입니다.\n");
 	    }
 
 	    System.out.println("<< 결과 출력 >>");
